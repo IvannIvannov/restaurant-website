b7ed113a9effd535a814456947638721a915daab
IvannIvannov <151462104+IvannIvannov@users.noreply.github.com>
2026-09-25 12:58:18 +0300

    update global accent color to green

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Cormorant_Garamond, Manrope } from "next/font/google";
 
-import "./globals.css";
+import "./global.css";
 
 const displayFont = Cormorant_Garamond({
   subsets: ["latin", "cyrillic"],
